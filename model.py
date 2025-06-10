4beb9dfac2fefb81e2897f912062658f1ecc6183
Srijit Kundu <srijit2004@gmail.com>
2025-06-10 21:53:07 +0530

    Refactor model.py to remove FFT layer and add new plot_tsne.py and plot_tsne_direct.py for t-SNE visualization

--- model.py
+++ model.py
@@ -2,21 +2,15 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Conv2D, MaxPooling2D, Input, Lambda
 import tensorflow as tf        
 
-def fft_layer(x):
-    """Applies 2D FFT to each channel independently and returns the magnitude spectrum."""
-    x = tf.cast(x, tf.complex64)  # Convert real to complex
-    fft = tf.signal.fft2d(x)  # Compute 2D FFT per channel
 
-    fft_magnitude = tf.abs(fft)  # Use magnitude only
-    return fft_magnitude
 
 def LPQ_net(in_shape=(256,256,3), num_classes=2):
     model = Sequential()
 
     model.add(Input(shape=in_shape))
 
-    # Apply FFT as the first layer
-    model.add(Lambda(fft_layer, name="fft_layer"))
+ 
+    
 
     model.add(Conv2D(32, kernel_size=(3, 3), name='convRes', activation='relu'))
 
