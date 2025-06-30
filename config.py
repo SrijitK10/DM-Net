01f09d424cc2a6793acd3ef5a8bc61ff4b505177
Srijit Kundu <srijit2004@gmail.com>
2025-06-30 11:48:48 +0530

    Update data directory path in config and replace confusion matrix and ROC curve images

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import torch
 
 class Config:
-    data_dir = "/Users/srijit/Documents/Projects Personal/FREQUENCY/GANDCTAnalysis/processed_dct_log_scaled"  # Update this
+    data_dir = "/Users/srijit/Documents/Projects Personal/FREQUENCY/GANDCTAnalysis/processed_dct_log_scaled/Protocol6_Mixed/3dgan" 
+  # Update this
     batch_size = 32
     num_epochs = 20
     learning_rate = 1e-4
