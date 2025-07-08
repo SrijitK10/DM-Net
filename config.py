367e813b89676540a6d7e44ace43876d0bb096d7
Srijit Kundu <srijit2004@gmail.com>
2025-07-08 15:12:28 +0530

    Update configuration and data loading: change data directory to './deepfake-eval', update checkpoint path, and enhance data loader with image transformations.

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import torch
 
 class Config:
-    data_dir = "/Users/srijit/Documents/Projects Personal/FREQUENCY/GANDCTAnalysis/processed_dct_log_scaled/Protocol6_Mixed/3dgan" 
+    data_dir = "./deepfake-eval" 
   # Update this
     batch_size = 32
     num_epochs = 20
     learning_rate = 1e-4
-    checkpoint_path = "./models/swin_t_scratch_DCT.pth"
+    checkpoint_path = "./models/best_swin_tiny_scratch_raw.pth"
     num_classes = 2  # Binary classification
     device = torch.device("mps")
--- data_loader.py
+++ data_loader.py
@@ -1,44 +1,44 @@
-import os
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-from torch.utils.data import DataLoader
-from config import Config
-from from_numpy import NumpyDataset
-
-def get_dataloaders():
-    
-
-    # train_dataset = NumpyDataset(os.path.join(Config.data_dir, "train"))
-    # val_dataset = NumpyDataset(os.path.join(Config.data_dir, "val"))
-    test_dataset = NumpyDataset(os.path.join(Config.data_dir, "test"))
-
-    # train_loader = DataLoader(train_dataset, batch_size=Config.batch_size, shuffle=True, num_workers=4)
-    # val_loader = DataLoader(val_dataset, batch_size=Config.batch_size, shuffle=False, num_workers=4)
-    test_loader = DataLoader(test_dataset, batch_size=Config.batch_size, shuffle=False, num_workers=4)
-
-    return test_loader
-
 # import os
 # import torchvision.transforms as transforms
 # import torchvision.datasets as datasets
 # from torch.utils.data import DataLoader
 # from config import Config
+# from from_numpy import NumpyDataset
 
 # def get_dataloaders():
-#     transform = transforms.Compose([
-#         transforms.Resize((224, 224)),
-#         transforms.ToTensor(),
-#     ])
+    
 
-#     # train_dataset = datasets.ImageFolder(os.path.join(Config.data_dir, "train"), transform=transform)
-#     # val_dataset = datasets.ImageFolder(os.path.join(Config.data_dir, "val"), transform=transform)
-#     test_dataset = datasets.ImageFolder(os.path.join(Config.data_dir, "test"), transform=transform)
+#     # train_dataset = NumpyDataset(os.path.join(Config.data_dir, "train"))
+#     # val_dataset = NumpyDataset(os.path.join(Config.data_dir, "val"))
+#     test_dataset = NumpyDataset(os.path.join(Config.data_dir, "test"))
 
 #     # train_loader = DataLoader(train_dataset, batch_size=Config.batch_size, shuffle=True, num_workers=4)
 #     # val_loader = DataLoader(val_dataset, batch_size=Config.batch_size, shuffle=False, num_workers=4)
 #     test_loader = DataLoader(test_dataset, batch_size=Config.batch_size, shuffle=False, num_workers=4)
 
 #     return test_loader
+
+import os
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+from config import Config
+
+def get_dataloaders():
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+
+    # train_dataset = datasets.ImageFolder(os.path.join(Config.data_dir, "train"), transform=transform)
+    # val_dataset = datasets.ImageFolder(os.path.join(Config.data_dir, "val"), transform=transform)
+    test_dataset = datasets.ImageFolder(os.path.join(Config.data_dir, "test"), transform=transform)
+
+    # train_loader = DataLoader(train_dataset, batch_size=Config.batch_size, shuffle=True, num_workers=4)
+    # val_loader = DataLoader(val_dataset, batch_size=Config.batch_size, shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=Config.batch_size, shuffle=False, num_workers=4)
+
+    return test_loader
 
 
 
